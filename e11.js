d0fb648465500c9f9411b14a55229e85c1184510
sahitiGanta <sahitiganta22@gmail.com>
2022-06-30 01:27:47 +0530

    updated on 29-06-2022

---
+++ e11.js
@@ -0,0 +1,8 @@
+// to import any module we use require 
+var os = require('os')
+console.log(os.hostname());
+console.log(os.type());
+console.log(os.loadavg());
+console.log(os.networkInterfaces());
+console.log(os.uptime());
+console.log(os.totalmem());
--- exp1.js
+++ exp1.js
@@ -1,2 +1,8 @@
 input = "webmaster"
 console.log(input.split('').sort().join(''))
+
+console.log("Example node js application")
+//Addition of 2 numbers
+var a=10,b=10;
+var c=a+b;
+console.log(c);
